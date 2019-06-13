bab9964b82f1582c74cbe41cd27e392f785fbf2b
JaybeeGeek <johnboscoobiejesi@gmail.com>
2019-06-13 09:21:09 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+
+
+document.getElementById('img').addEventListener('click', e => {
+    const disObj = disintegrate.getDisObj(e.target)
+    disintegrate.createSimultaneousParticles(disObj)
+    e.target.remove()
+})
+disintegrate.init()
+
